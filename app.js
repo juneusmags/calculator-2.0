c5c17a7ac0e80386c3c168be1526c9eb208b363c
Juneus Magsakay <jbm9397@gmail.com>
2022-06-15 12:23:55 -0400

    INITIALCOMMIT

---
+++ app.js
@@ -0,0 +1,125 @@
+//ELEMENTS
+var allNumbers = document.querySelectorAll("#number")
+var allOperations = document.querySelectorAll("#operation")
+var equals = document.querySelector("#equals")
+var deleteSingle = document.querySelector("#delete")
+var prevDisplay = document.querySelector("#previous-display")
+var currentDisplay = document.querySelector("#current-display")
+var clearAll = document.querySelector("#clear")
+
+//VARIABLES
+let operater = ""
+let currentNumber = ""
+let prevNumber = ""
+
+//CLEARS ALL VARIABLES
+const clear = () => {
+    operater = ""
+    currentNumber = ""
+    prevNumber = ""
+}
+//DELETES END NUMBER
+const backSpace = () => {
+    currentNumber = currentNumber.toString().slice(0, -1)
+}
+//INPUTS A NUMBER FOR CALCULATION
+const inputNumber = (number) => {
+    //CHECKS IF NUMBER ALREADY CONTAINS A .
+    if(number === "." && currentNumber.includes(".")){
+        return
+    }
+    //SETS INPUT NUMBER TO CURRENTNUMBER AS A STRING
+    currentNumber = currentNumber.toString() + number
+}
+//SELECTS OPERATER
+const selectOperater = (operation) => {
+    if(currentNumber === ""){
+        return
+    }
+    if(prevNumber !== ""){
+        calculate()
+    }
+
+    operater = operation
+    prevNumber = currentNumber
+    currentNumber = ""
+}
+//DOES THE CALCULATIONS
+const calculate = () => {
+    let result = ""
+    let previous = parseFloat(prevNumber)
+    let current = parseFloat(currentNumber)
+    //CHECKS IF YOU ARE DIVIDING BY 0 / FROM A 0
+    if(operater === "÷" && current === 0 || previous === 0){
+        alert("You cant do that")
+    }
+    else{
+        //CHECKS IF PREVIOUS OR CURRENT NUMBER EXISTS
+        if(!previous || !current){
+            return
+        }
+    
+        if(operater === "+"){
+            
+            result = previous + current
+        }
+        else if(operater === "-"){
+            result = previous - current
+        }
+        else if(operater === "X"){
+            result = previous*current
+        }
+        else if(operater === "÷"){
+            
+            result = previous / current
+        }
+        else{
+            return
+        }
+    
+        if(result % 1 !== 0){
+            result = Math.round(result)
+        }
+        
+        currentNumber = result
+        operater = ""
+        prevNumber = "" 
+    }
+    
+}
+//UPDATES THE DISPLAY
+const updateDisplay = () => {
+    currentDisplay.innerHTML = currentNumber
+    prevDisplay.innerHTML = prevNumber
+}
+
+//EVENT LISTENERS
+allNumbers.forEach((numberBtn) => {
+    numberBtn.addEventListener("click", () => {
+        inputNumber(numberBtn.innerHTML)
+        updateDisplay()
+    })
+})
+
+allOperations.forEach((operation) => {
+    operation.addEventListener("click", () => {
+        selectOperater(operation.innerHTML)
+        updateDisplay()
+    })
+})
+
+equals.addEventListener("click", () => {
+    calculate()
+    updateDisplay()
+})
+
+
+clearAll.addEventListener("click", () => {
+    clear()
+    updateDisplay()
+}) 
+
+deleteSingle.addEventListener("click", ()=>{
+    backSpace()
+    updateDisplay()
+})
